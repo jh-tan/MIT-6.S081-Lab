2713e6ee661f7bb7844246b02237d08d6e726e14
jh-tan <hui971105@gmail.com>
2021-04-30 21:48:19 +0800

    Add solution for xargs

---
+++ user/xargs.c
@@ -0,0 +1,59 @@
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../kernel/param.h"
+#include "user.h"
+
+/*
+ *int main(int argc, char *argv[]) {
+ *    char *exec = argv[1];
+ *    exit(0);
+ *}
+ */
+
+#define MAX 1024
+int main(int argc, char *argv[])
+{
+    //example echo hello too | xargs echo bye
+    char line[MAX];
+    char* params[MAXARG];
+    int n, args_index = 0;
+    int i;
+    //argv[0] = xargs
+    //argv[1] = echo
+    //argv[2] = bye
+    //argv[3] = null
+    //cmd = echo
+    char* cmd = argv[1];
+    for (i = 1; i < argc; i++) params[args_index++] = argv[i];
+
+    //params[0] = echo
+    //params[1] = bye
+
+    // Read stand input
+    while ((n = read(0, line, MAX)) > 0)
+    {
+        //read : hello too
+        if (fork() == 0) // child process
+        {
+            int index = 0;
+            char *p;
+            for (p = line; *p !='\0' ;p++)
+            {
+                if (*p == ' ' || *p == '\n')
+                {
+                    *p = '\0';
+                    params[args_index++] = p-index;
+                    index = 0;
+                    //params[2] = hello
+                    //params[3] = too
+                }
+                else
+                    index++;
+            }
+            params[args_index] = 0;
+            exec(cmd, params);
+        }
+        else wait((int*)0);
+    }
+    exit(0);
+}
